feb3a76a12525fb3d3a006bdaf0574f77ce8d505
CARLOS HENRIQUE CASTRO E SOUSA <carloshenriquemg2020@gmail.com>
2023-03-07 18:31:49 -0300

    vh

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -3,7 +3,7 @@ import styles from "./About.module.css";
 
 const About = () => {
   return (
-    <section className={`${styles.about} container`}>
+    <section id="about" className={`${styles.about} container`}>
       <h1 className="title">Sobre mim</h1>
       <p className={styles.subtitle}>
         Aqui você encontrará mais informações sobre mim, o que faço e minhas
